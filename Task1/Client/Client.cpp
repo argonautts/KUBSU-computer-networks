12db4980d90b9be21ffc764a01319fbd50279f17
Alexandr Kozin <sasha.kozin.2014alex444@ya.ru>
2022-01-14 22:08:28 +0300

    Task 1

---
+++ Task1/Client/Client.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+struct Person
+{
+    char name[25];
+    int math;
+    int russ;
+    int inf;
+} A;
+void main() {
+    setlocale(LC_ALL, "rus");
+    const char* nameR = "C:\\Users\\ALEKSANDR\\source\\repos\\Seti\\Debug\\server.bin ";//файл для сервера
+    const char* nameA = "C:\\Users\\ALEKSANDR\\source\\repos\\Seti\\Debug\\second.bin";//файл для клиента
+    const char* nameQ = "C:\\Users\\ALEKSANDR\\source\\repos\\Seti\\Debug\\queue.bin ";//файл с номером очереди
+    ofstream f_REQ, fr;
+    ifstream f_ANS, fa;
+    long pred_size;
+    long size_predQueue;
+    int answer, k;
+    while (true)
+    {
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+        cout << "Введите Фамилию и баллы по 3 предметам: Математика, Русский, Информатика" << endl;
+        cin >> A.name >> A.math >> A.russ >> A.inf;
+        cout << "Ответ для пользователя " << A.name << endl;
+        cout << "Учени(к/ца) " << A.name << " с баллами по Математике " << A.math << ", Руссикий язык " << A.russ << " и Информатике " << A.inf << endl;
+
+        f_REQ.open(nameR, ios::app | ios::binary);//запись в файл
+        f_REQ.write((char*)&A, sizeof(A));
+        f_REQ.close();
+
+        f_ANS.open(nameA, ios::binary);
+        fa.open(nameQ, ios::binary);
+        if (!f_ANS.is_open() || !fa.is_open())//Если с файлом что-то не так выводим ERROR
+            cout << "ERROR" << endl;
+        else
+        {
+            f_ANS.seekg(0, ios::end);
+            fa.seekg(0, ios::end);
+            size_predQueue = fa.tellg();
+            pred_size = f_ANS.tellg();
+            while (pred_size >= f_ANS.tellg())
+            {
+                Sleep(100);
+                f_ANS.seekg(0, ios::end);
+            }
+            while (size_predQueue >= fa.tellg())
+            {
+                Sleep(100);
+                fa.seekg(0, ios::end);
+            }
+            f_ANS.seekg(pred_size, ios::beg);
+            fa.seekg(size_predQueue, ios::beg);
+            f_ANS.read((char*)&answer, sizeof(answer));
+            fa.read((char*)&k, sizeof(k));
+            fa.close();
+            f_ANS.close();
+        }
+        switch (answer) {
+            case 0: {cout << "Ученик отлично сдал экзамены!" << endl; break; }
+            case 1: {cout << "Ученик хорошо сдал экзамены! " << endl; break; }
+            case 2: {cout << "Ученик средне сдал экзамены! " << endl; break; }
+            case 3: {cout << "Ученик плохо сдал экзамены! " << endl; break; }
+            case 4: {cout << "Ученик не сдал экзамены! " << endl; };
+
+        }
+    }
+}
---
+++ Task1/Server/Server.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+
+struct Person
+{
+    char name[25];
+    int math;
+    int russ;
+    int inf;
+}B;
+
+int k = 0;
+double ball = 0;//место и бал
+int answer;//ответ
+long size_pred;
+
+int main() {
+    ifstream fR;
+    ofstream fA;
+    ofstream f;
+    f.open("C:\\Users\\user\\source\\repos\\ConsoleApplication12\\ConsoleApplication12\\queue.bin");
+    f.close();
+    setlocale(LC_ALL, "rus");
+    const char* nameR = "C:\\Users\\ALEKSANDR\\source\\repos\\Seti\\Debug\\server.bin ";//файл запросов клиентов
+    const char* nameA = "C:\\Users\\ALEKSANDR\\source\\repos\\Seti\\Debug\\second.bin";//файл ответов сервера
+    const char* nameQ = "C:\\Users\\ALEKSANDR\\source\\repos\\Seti\\Debug\\queue.bin ";//файл  записи на очередь
+
+    cout << "server is working" << endl;
+    fR.open(nameR, ios::binary);//открытие файла REQUEST
+    fR.seekg(0, ios::end);
+    size_pred = fR.tellg();//стартовая позиция сервера в файле REQUEST
+    fR.close();
+    while (true)// начало работы сервера
+    {
+        fR.open(nameR, ios::binary);
+        fR.seekg(0, ios::end);
+        while (size_pred >= fR.tellg())
+        {
+            Sleep(100); fR.seekg(0, ios::end);
+        }
+        fR.seekg(size_pred, ios::beg);//переход к началу полученного запроса
+        fR.read((char*)&B, sizeof(B));//считывание данных клиента
+
+        cout << "Сервер работает с учени(ком/цой) " << B.name << endl;
+        size_pred = fR.tellg();
+        fR.close();
+        ball = (B.math + B.russ + B.inf)/3;//Считаем сумму баллов
+        //В 5 if'aх определяем как ученик сдал экзамен
+        if (ball>=90) {
+            answer = 0;
+            k++;
+         }
+        if (ball>=80 && ball < 90) {
+            answer = 1;
+            k++;
+        }
+        if (ball >= 60 && ball < 80) {
+            answer = 2;
+            k++;
+        }
+        if (ball >= 40 && ball < 60) {
+            answer = 3;
+            k++;
+        }
+        if (ball >= 0 && ball < 40) {
+            answer = 4;
+            k++;
+        }
+        fA.open(nameA, ios::binary | ios::app);//открытие файла ANSWER и запись в конец 
+        fA.write((char*)&answer, sizeof(answer));//запись ответа клиенту
+        fA.close();
+        fA.open(nameQ, ios::binary | ios::app);//открытие файла очереди
+        fA.write((char*)&k, sizeof(ball));//запись баллов
+        fA.write((char*)&k, sizeof(k));//запись номера ученика
+        fA.close();
+        cout << "Сервер закончил работу с учени(ком/цой): " << B.name << " его/её средний балл = " << ball<< " №" << k << endl;
+        ball = 0;
+    }// while
+}
